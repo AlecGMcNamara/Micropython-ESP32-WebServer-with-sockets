c4350cbd4f7468a25b437c4f8806dde57a858c3e
Alec McNamara <alecgmcnamara@gmail.com>
2023-05-03 11:00:26 +0100

    Add files via upload

---
+++ static/index.js
@@ -0,0 +1,32 @@
+// WebSocket support
+var targetUrl = `ws://${location.host}/ws`;
+var websocket;
+window.addEventListener("load", onLoad);
+
+function onLoad() {
+  initializeSocket();
+}
+
+function initializeSocket() {
+  console.log("Opening WebSocket connection MicroPython Server...");
+  websocket = new WebSocket(targetUrl);
+  websocket.onopen = onOpen;
+  websocket.onclose = onClose;
+  websocket.onmessage = onMessage;
+}
+function onOpen(event) {
+  console.log("Starting connection to WebSocket server..");
+}
+function onClose(event) {
+  console.log("Closing connection to server..");
+  setTimeout(initializeSocket, 2000);
+}
+function onMessage(event) {
+  console.log("WebSocket message received:", event.data);
+  document.getElementById("myMessage").innerHTML = event.data;
+  sendMessage("You sent " + event.data)
+}
+
+function sendMessage(message) {
+  websocket.send(message);
+}
